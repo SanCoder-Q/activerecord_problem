eec08ac5ef233d7d8de6f8a38590622021f9b1c7
SanCoder-Q <sancoder.q@gmail.com>
2014-12-22 21:15:45 +0800

    refactor

--- app.rb
+++ app.rb
@@ -11,8 +11,18 @@ class ProblemApp
 	puts "Running... "
 
 	user = User.first
-	(1..10).each do |i|
-		user.update_book 1, i
-	end
+	(1..10).each do |price|
+		user.books.each do |book|	####### bug line !!!!!!!!!!!!!!!
+		end						####### bug line !!!!!!!!!!!!!!!
 
+		return nil unless (book = user.books.find_by_id 1)
+		puts book.name.to_s + " + " + price.to_s
+
+		book.price = price	# change attr
+		puts book.save		# save changing
+
+		book = user.books.first
+		puts book.name.to_s + " - " + book.price.to_s
+		puts "================="
+	end
 end
--- models/user.rb
+++ models/user.rb
@@ -1,21 +1,3 @@
 class User < ActiveRecord::Base
 	has_many :books, foreign_key: "user_id", dependent: :destroy
-
-	def update_book book_id, price
-
-		books.each do |book|	# bug line
-		end					# bug line
-
-		return nil unless (book = books.find_by_id book_id)
-		puts book.name.to_s + " + " + price.to_s
-
-		book.price = price
-		puts book.save
-
-		book = books.first
-		puts book.name.to_s + " - " + book.price.to_s
-		puts "================="
-
-	end
-
 end
